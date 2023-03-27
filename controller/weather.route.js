1c2cccc7e9711efd648916bd300e1bc960ecba9e
KanadeAditya <110182805+KanadeAditya@users.noreply.github.com>
2023-03-27 16:58:44 +0530

    c_4

--- controller/weather.route.js
+++ controller/weather.route.js
@@ -32,7 +32,7 @@ weatherrouter.get("/:city",async (req,res)=>{
             res.send(JSON.parse(ifexist));
         }else{
             let data = await fetch(`${process.env.weather}&q=${city}&aqi=no`).then((res)=>res.json());
-            client.hSet("weather",city,JSON.stringify(data),"EX","2")
+            client.hSet("weather",city,JSON.stringify(data))
             res.send(data)
         }
     } catch (error) {
